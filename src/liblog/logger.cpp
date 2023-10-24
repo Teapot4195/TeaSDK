d6ca6e845f2779f24df2d02b0ab1684bd8dd6b6c
Alex Huang <huangalex409@gmail.com>
2023-10-24 14:25:09 -0400

    Implement better logging

--- src/liblog/logger.cpp
+++ src/liblog/logger.cpp
@@ -3,3 +3,105 @@
 //
 
 #include "logger.h"
+
+const std::string default_logger_name = "GLOBAL";
+std::ostream& default_logger_stream = std::cout;
+
+namespace liblog {
+    /// Global Logger instance
+    console_logger global_logger_;
+
+    /// Mutex for the global logger instance, here for multithreading
+    std::mutex log_mutex_;
+
+    logger::~logger() = default;
+
+    console_logger::~console_logger() = default;
+
+    console_logger::console_logger() :
+        name(default_logger_name), level(log_level::INFO), stream(default_logger_stream) {}
+
+    console_logger::console_logger(const std::string &name) :
+        name(name), level(log_level::INFO), stream(default_logger_stream) {}
+
+    console_logger::console_logger(enum log_level level) :
+        name(default_logger_name), level(level), stream(default_logger_stream) {}
+
+    console_logger::console_logger(const std::string &name, enum log_level level) :
+        name(name), level(level), stream(default_logger_stream) {}
+
+    console_logger::console_logger(const std::string &name, enum log_level level, std::ostream &stream) :
+        name(name), level(level), stream(stream) {}
+
+    void console_logger::trace(const std::string &str) {
+        if (level > log_level::TRACE)
+            return;
+
+        stream << "TRACE: " << name << ": " << str << std::endl;
+    }
+
+    void console_logger::verbose(const std::string &str) {
+        if (level > log_level::VERBOSE)
+            return;
+
+        stream << "VERBOSE: " << name << ": " << str << std::endl;
+    }
+
+    void console_logger::debug(const std::string &str) {
+        if (level > log_level::DEBUG)
+            return;
+
+        stream << "DEBUG: " << name << ": " << str << std::endl;
+    }
+
+    void console_logger::info(const std::string &str) {
+        if (level > log_level::INFO)
+            return;
+
+        stream << "INFO: " << name << ": " << str << std::endl;
+    }
+
+    void console_logger::warn(const std::string &str) {
+        if (level > log_level::WARN)
+            return;
+
+        stream << "WARN: " << name << ": " << str << std::endl;
+    }
+
+    void console_logger::fatal(const std::string &str) {
+        if (level > log_level::FATAL)
+            return;
+
+        stream << "FATAL: " << name << ": " << str << std::endl;
+    }
+}
+
+void trace(const std::string& str) {
+std::lock_guard<std::mutex> log(liblog::log_mutex_);
+liblog::global_logger_.trace(str);
+}
+
+void verbose(const std::string& str) {
+std::lock_guard<std::mutex> log(liblog::log_mutex_);
+liblog::global_logger_.verbose(str);
+}
+
+void debug(const std::string& str) {
+std::lock_guard<std::mutex> log(liblog::log_mutex_);
+liblog::global_logger_.debug(str);
+}
+
+void info(const std::string& str) {
+std::lock_guard<std::mutex> log(liblog::log_mutex_);
+liblog::global_logger_.info(str);
+}
+
+void warn(const std::string& str) {
+std::lock_guard<std::mutex> log(liblog::log_mutex_);
+liblog::global_logger_.warn(str);
+}
+
+void fatal(const std::string& str) {
+std::lock_guard<std::mutex> log(liblog::log_mutex_);
+liblog::global_logger_.fatal(str);
+}
--- src/liblog/logger.h
+++ src/liblog/logger.h
@@ -8,21 +8,210 @@
 #include <mutex>
 #include <iostream>
 
+/**
+ * @brief main namespace for liblog
+ *
+ * All of the logging library resides here, except for the public facing log functions
+ */
 namespace liblog {
+    /**
+     * @brief level of depth for the logs
+     */
+    enum log_level {
+        TRACE = 0,   ///< Trace level, LITERALLY EVERYTHING
+        VERBOSE = 1, ///< Verbose level, usually less stuff
+        DEBUG = 2,   ///< Debug info, even less stuff
+        INFO = 3,    ///< Status messages and more important only
+        WARN = 4,    ///< Warning messages and more important only
+        FATAL = 5,   ///< pretty much nothing, only fatal errors will be logged
+        NONE = 6     ///< None level, LITERALLY ABSOLUTELY NOTHING
+    };
+
+    /**
+     * @brief base logger class
+     */
     class logger {
     public:
-        void warn(std::string str);
-    } global_logger_;
+        virtual ~logger();
 
-    std::mutex log_mutex_;
+        /**
+         * @brief logs a trace level message
+         *
+         * @param str message to log
+         */
+        virtual void trace(const std::string& str) = 0;
+
+        /**
+         * @brief logs a verbose level message
+         *
+         * @param str message to log
+         */
+        virtual void verbose(const std::string& str) = 0;
+
+        /**
+         * @brief logs a debug level message
+         *
+         * @param str message to log
+         */
+        virtual void debug(const std::string& str) = 0;
+
+        /**
+         * @brief logs a info level message
+         *
+         * @param str message to log
+         */
+        virtual void info(const std::string& str) = 0;
+
+        /**
+         * @brief logs a warning level message
+         *
+         * @param str warning to log
+         */
+        virtual void warn(const std::string& str) = 0;
+
+        /**
+         * @brief logs a fatal level message
+         *
+         * @param str message to log
+         */
+        virtual void fatal(const std::string& str) = 0;
+    };
+
+    /**
+     * @brief console logger implementation
+     */
+    class console_logger : public logger {
+    public:
+        ~console_logger() override;
+
+        /**
+         * @brief builds a logger instance with the class name of "GLOBAL"
+         */
+        console_logger();
+
+        /**
+         * @brief builds a logger instance with the specified class name
+         *
+         * @param name class name to log under
+         */
+        explicit console_logger(const std::string& name);
+
+        /**
+         * @brief builds a logger instance with the specified log level
+         *
+         * @param level level to log at
+         */
+        explicit console_logger(log_level level);
+
+        /**
+         * @brief builds a logger instance with the specified name and log level
+         *
+         * @param name name of the class to log under
+         * @param level level to log at
+         */
+        console_logger(const std::string& name, log_level level);
+
+        /**
+         * @brief builds a logger instance with the specified name, log level and output stream
+         *
+         * @param name name of the class to log under
+         * @param level level to log at
+         */
+        console_logger(const std::string& name, log_level level, std::ostream& stream);
+
+        /**
+         * @brief logs a trace level message
+         *
+         * @param str message to log
+         */
+        void trace(const std::string& str) override;
+
+        /**
+         * @brief logs a verbose level message
+         *
+         * @param str message to log
+         */
+        void verbose(const std::string& str) override;
+
+        /**
+         * @brief logs a debug level message
+         *
+         * @param str message to log
+         */
+        void debug(const std::string& str) override;
+
+        /**
+         * @brief logs a info level message
+         *
+         * @param str message to log
+         */
+        void info(const std::string& str) override;
+
+        /**
+         * @brief logs a warning level message
+         *
+         * @param str warning to log
+         */
+        void warn(const std::string& str) override;
+
+        /**
+         * @brief logs a fatal level message
+         *
+         * @param str message to log
+         */
+        void fatal(const std::string& str) override;
+
+    private:
+        const std::string& name;
+        std::ostream& stream;
+        log_level level;
+    };
+
+    class color_console_logger : public console_logger {
+
+    };
 }
 
-void warn(const std::string& str) {
-    //TODO: actually call the correct logger function in the future
-    std::lock_guard<std::mutex> log(liblog::log_mutex_);
-    //liblog::global_logger_.warn(str);
+/**
+ * @brief logs a trace message to the console
+ *
+ * @param str the trace message to log
+ */
+void trace(const std::string& str);
 
-    std::cout << "WARN: " <<  str << std::endl;
-}
+/**
+ * @brief logs a verbose message to the console
+ *
+ * @param str the verbose message to log
+ */
+void verbose(const std::string& str);
+
+/**
+ * @brief logs a debug message to the console
+ *
+ * @param str the debug message to log
+ */
+void debug(const std::string& str);
+
+/**
+ * @brief logs a info message to the console
+ *
+ * @param str the info message to log
+ */
+void info(const std::string& str);
+
+/**
+ * @brief logs a warning to the console
+ *
+ * @param str the warning to log
+ */
+void warn(const std::string& str);
+
+/**
+ * @brief logs a fatal message to the console
+ *
+ * @param str the fatal message to log
+ */
+void fatal(const std::string& str);
 
 #endif //TEASDK_LOGGER_H
